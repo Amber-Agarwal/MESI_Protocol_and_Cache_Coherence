f5d13dfec3dc6b14c8b3c011c648dd4636f653bc
Amber-Agarwal <amberagarwalmusic@gmail.com>
2025-04-24 23:41:33 +0530

    see what i have done

--- cache.cpp
+++ cache.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <iomanip>
 #include <bitset>
+#include <getopt.h>
 
 using namespace std;
 
@@ -46,7 +47,7 @@ public:
     bool stall_flag = false;
     int current_instruction_number = 0;
     // Constructor
-    Cache(int set_bits, int num_ways, int cache_line_bits) {
+    Cache(int set_bits, int num_ways, int cache_line_bits,string filepath) {
         num_sets = (1<<set_bits);
         this->set_bits = set_bits;
         assosciativity = num_ways;
@@ -141,5 +142,81 @@ public:
     }
 };
   
+  
+int main(int argc, char* argv[]) {
+    string tracefile = "default_trace.txt"; // Default trace file
+    int s = 6;                             // Default set index bits
+    int E = 4;                             // Default associativity
+    int b = 6;                             // Default block bits
+    string outfilename = "default_output.txt"; // Default output file
 
+    int opt;
+    while ((opt = getopt(argc, argv, "t:s:E:b:o:h")) != -1) {
+        switch (opt) {
+            case 't':
+                tracefile = optarg;
+                break;
+            case 's':
+                s = stoi(optarg);
+                break;
+            case 'E':
+                E = stoi(optarg);
+                break;
+            case 'b':
+                b = stoi(optarg);
+                break;
+            case 'o':
+                outfilename = optarg;
+                break;
+            case 'h':
+                cout << "Usage: " << argv[0] << " -t <tracefile> -s <s> -E <E> -b <b> -o <outfilename> -h" << endl;
+                cout << "  -t <tracefile>: name of parallel application (e.g. app1) whose 4 traces are to be used in simulation" << endl;
+                cout << "  -s <s>: number of set index bits (number of sets in the cache = S = 2^s)" << endl;
+                cout << "  -E <E>: associativity (number of cache lines per set)" << endl;
+                cout << "  -b <b>: number of block bits (block size = B = 2^b)" << endl;
+                cout << "  -o <outfilename>: logs output in file for plotting etc." << endl;
+                cout << "  -h: prints this help" << endl;
+                return 0;
+            default:
+                cerr << "Usage: " << argv[0] << " -t <tracefile> -s <s> -E <E> -b <b> -o <outfilename> -h" << endl;
+                return 1;
+        }
+    }
 
+    // Now you can use tracefile, s, E, b, and outfilename in your cache simulation
+
+    // cout << "Tracefile: " << tracefile << endl;
+    // cout << "Set index bits: " << s << endl;
+    // cout << "Associativity: " << E << endl;
+    // cout << "Block bits: " << b << endl;
+    // cout << "Output filename: " << outfilename << endl;
+
+    // Cache my_cache(s,E,b);
+    // vector<pair<operation, string>> trace_data;
+    // my_cache.process_trace_file(tracefile,trace_data);
+    // for(auto &[op,address]:trace_data){
+    //     cout<<"Operation: "<<(op==operation::R?"Read":"Write")<<", Address: "<<address<<endl;
+    //     Bits bits = my_cache.parse(address);
+    //     miss_or_hit result = my_cache.hit_or_miss(bits);
+
+    //     if (result == miss_or_hit::HIT) {
+    //         cout << "Cache Hit!" << endl;
+    //     } else {
+    //         cout << "Cache Miss!" << endl;
+    //     }
+    // }
+
+        // Construct the trace file paths based on the tracefile name
+        string trace_path_0 = "traces/" + tracefile + "_proc0.trace";
+        string trace_path_1 = "traces/" + tracefile + "_proc1.trace";
+        string trace_path_2 = "traces/" + tracefile + "_proc2.trace";
+        string trace_path_3 = "traces/" + tracefile + "_proc3.trace";
+
+    Cache cache0(s,E,b,trace_path_0);
+    Cache cache1(s,E,b,trace_path_1);
+    Cache cache2(s,E,b,trace_path_2);
+    Cache cache3(s,E,b,trace_path_3);
+    
+    
+    return 0;
+}
