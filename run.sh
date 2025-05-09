896170a343f18e33fadc64d3b8f0d80605dc5849
Anoop Singh <cs1230459@iitd.ac.in>
2025-05-09 16:17:10 +0530

    final

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clang++ cache.cpp
+./a.out -t app1 > outputs/output1.txt
+./a.out -t app2 > outputs/output2.txt
 ./a.out -t app3 > outputs/output3.txt
 ./a.out -t app4 -s 0 -E 1 > outputs/output4.txt
 ./a.out -t app5 > outputs/output5.txt
